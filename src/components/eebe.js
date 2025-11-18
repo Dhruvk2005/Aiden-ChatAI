a37fb2a883d6a4af4460abbd5e272f0c61339b66
Dhruv Khade <dhruvkhade05@gmail.com>
2025-11-19 00:29:16 +0530

    dnaihv

---
+++ src/components/eebe.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const eebe = () => {
+
+    const[delay,setDelay] = useState()
+  return (
+    <div>
+      hello how are you guys
+    </div>
+  )
+}
+
+export default eebe
